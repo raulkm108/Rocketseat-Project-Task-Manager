039f36d64908da19fc30da61e40c6a89cd0e1502
raulkm108 <raulkm.civil@gmail.com>
2025-06-18 09:31:30 -0300

    Creating the front menu
    Creating the options menu, with the break in case the user types 6, which leaves the menu and terminates the program

---
+++ main.py
@@ -0,0 +1,15 @@
+while True:
+    print("\nTask manager menu:")
+    print("1. Add task")
+    print("2. See tasks")
+    print("3. Update task")
+    print("4. Complete task")
+    print("5. Delete completed tasks")
+    print("6. Leave")
+
+    choice = input("Type your choice: ")
+
+    if choice == "6":
+        break
+
+print("Program Finished")
